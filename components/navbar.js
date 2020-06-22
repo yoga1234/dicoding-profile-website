d07d479e55653888d2cf822e5efae1fc3e8e7045
Septiana Yoga <septianayoga30@gmail.com>
2020-06-22 14:21:56 +0700

    adding page files

--- components/navbar.js
+++ components/navbar.js
@@ -6,10 +6,10 @@ const NAVBAR = `
       <a href="#" class="brand-logo">Yogasmara</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><h5>☰</h5></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="home">Home</a></li>
-        <li><a href="skill">Skill</a></li>
-        <li><a href="testimonials">Testimonials</a></li>
-        <li><a href="about">About</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#skill">Skill</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#about">About</a></li>
       </ul>
     </div>
   </nav>
---
+++ components/testimonials/testimonials.html
@@ -0,0 +1,1 @@
+<h2>Testimonials</h2>
