75831a41cf6f030f553683812c47e64cc312583a
Septiana Yoga <septianayoga30@gmail.com>
2020-06-23 16:02:59 +0700

    update skill, testi page and add image

--- components/skill/skill.js
+++ components/skill/skill.js
@@ -2,12 +2,22 @@ const SKILL_PAGE = () => {
   window.location.hash = "skill";
   return `
     <div class="container">
-      <div class="row">
+    <blockquote><h3>SKILLSET</h3></blockquote>
+    <h5>I'm experienced with :</h5>
+    <div class="row">
         <div class="col s6">
           <div class="card-panel">
-            <h5>Javascript</h5>
+            <h5>HTML</h5>
             <div class="progress">
-              <div class="indeterminate" style="width: 60%"></div>
+              <div class="determinate" style="width: 75%"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="card-panel">
+            <h5>CSS</h5>
+            <div class="progress">
+              <div class="determinate" style="width: 70%"></div>
             </div>
           </div>
         </div>
@@ -15,7 +25,31 @@ const SKILL_PAGE = () => {
           <div class="card-panel">
             <h5>Javascript</h5>
             <div class="progress">
-              <div class="determinate" style="width: 60%"></div>
+              <div class="determinate" style="width: 80%"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="card-panel">
+            <h5>ReactJS</h5>
+            <div class="progress">
+              <div class="determinate" style="width: 40%"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="card-panel">
+            <h5>NodeJS</h5>
+            <div class="progress">
+              <div class="determinate" style="width: 30%"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col s6">
+          <div class="card-panel">
+            <h5>UI/UX Design</h5>
+            <div class="progress">
+              <div class="determinate" style="width: 50%"></div>
             </div>
           </div>
         </div>
--- components/testimonials/testimonials.js
+++ components/testimonials/testimonials.js
@@ -1,6 +1,64 @@
 const TESTIMONIALS_PAGE = () => {
   window.location.hash = "testimonials";
-  return `<h2>Testimonials</h2>`
+  return `
+    <div class="container">
+      <h5 class="center-align">Testimonials</h5>
+      <div class="row">
+        <div class="col s8 offset-s2">
+          <div class="card-panel grey lighten-5 z-depth-1">
+            <div class="row valign-wrapper">
+              <div class="col s4">
+                <img src="../../images/testi-1-min.jpg" alt="" class="responsive-img valign"> <!-- notice the "circle" class -->
+              </div>
+              <div class="col s8">
+                <h6>"<i>Great job, I will definitely be ordering again! Website was worth a fortune to my company.</i>"</h6>
+                <blockquote>Jonathan Grand</blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s8 offset-s2">
+          <div class="card-panel grey lighten-5 z-depth-1">
+            <div class="row valign-wrapper">
+            <div class="col s8">
+              <h6>"<i>I will recommend you to my colleagues. It fits our needs perfectly.</i>"</h6>
+              <blockquote>Jack F.</blockquote>
+            </div>
+              <div class="col s4">
+                <img src="../../images/testi-2-min.jpg" alt="" class="responsive-img valign"> <!-- notice the "circle" class -->
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s8 offset-s2">
+          <div class="card-panel grey lighten-5 z-depth-1">
+            <div class="row valign-wrapper">
+              <div class="col s4">
+                <img src="../../images/testi-3-min.jpg" alt="" class="responsive-img valign"> <!-- notice the "circle" class -->
+              </div>
+              <div class="col s8">
+                <h6>"<i>I just can't get enough of website. I want to get a T-Shirt with website on it so I can show it off to everyone.</i>"</h6>
+                <blockquote>Anita X.</blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col s8 offset-s2">
+          <div class="card-panel grey lighten-5 z-depth-1">
+            <div class="row valign-wrapper">
+            <div class="col s8">
+              <h6>"<i>Great job, Yogasmara is the best developer I've ever know.</i>"</h6>
+              <blockquote>Alianora L.</blockquote>
+            </div>
+              <div class="col s4">
+                <img src="../../images/testi-4-min.jpg" alt="" class="responsive-img valign"> <!-- notice the "circle" class -->
+              </div>
+            </div>
+          </div>
+        </div> 
+      </div>
+    </div>
+  `;
 }
 
 export default TESTIMONIALS_PAGE;
