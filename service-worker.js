1923bdc9a052ae3ae2520fd1be163d530e18ed06
Septiana Yoga <septianayoga30@gmail.com>
2020-06-24 23:49:14 +0700

    add some script to serviceworker file

--- service-worker.js
+++ service-worker.js
@@ -10,13 +10,14 @@ var urlsToCache = [
   "/images/testi-2-min.jpg",
   "/images/testi-3-min.jpg",
   "/images/testi-4-min.jpg",
+  "/images/top-icon.png",
   "/css/main.css",
   "/css/materialize.min.css",
   "/components/footer.js",
   "/components/navbar.js",
   "/components/pageLoader.js",
   "/components/about/about.js",
-  "/components/home/home/js",
+  "/components/home/home.js",
   "/components/skill/skill.js",
   "/components/testimonials/testimonials.js"
 ];
@@ -25,6 +26,40 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function(response) {
+        if(response) {
+          console.log("serviceWorker: aset yang digunakan: ", response.url);
+          return response;
+        }
+
+        console.log(
+          "serviceWorker: memuat aset dari server",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if(cacheName != CACHE_NAME){
+            console.log("serviceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
